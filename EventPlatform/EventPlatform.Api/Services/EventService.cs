afadf89c901d15f7182641dccc8b990da8bfc701
Igor Baranov <ibaranov@fluxbit.ru>
2026-06-16 21:10:17 +0300

    Реализованы интерфейсы

---
+++ EventPlatform/EventPlatform.Api/Services/EventService.cs
@@ -0,0 +1,34 @@
+﻿using EventPlatform.Api.Interfaces;
+using EventPlatform.Api.Model;
+
+namespace EventPlatform.Api.Services;
+
+public class EventService(IStorage _context) : IEventService
+{
+    public void Add(Event obj)
+    {
+        if (obj.StartAt > obj.EndAt)
+            throw new ArgumentOutOfRangeException("Дата окончания не может быть раньше даты начала.");
+        _context.AddEvent(obj);
+    }
+
+    public void Delete(Guid id)
+    {
+        _context.DeleteEvent(id);
+    }
+
+    public IEnumerable<Event> GetAll()
+    {
+        return _context.GetAllEvents();
+    }
+
+    public Event GetById(Guid id)
+    {
+        return _context.GetEventById(id);
+    }
+
+    public void Update(Guid id, Event obj)
+    {
+        _context.UpdateEvent(id, obj);
+    }
+}
---
+++ EventPlatform/EventPlatform.Api/Storage/InMemoryStorage.cs
@@ -0,0 +1,47 @@
+﻿using EventPlatform.Api.Interfaces;
+using EventPlatform.Api.Model;
+
+namespace EventPlatform.Api.Storage;
+
+public class InMemoryStorage : IStorage
+{
+    List<Event> events { get; } = new List<Event>();
+    public void AddEvent(Event obj)
+    {
+        events.Add(obj);
+    }
+
+    public bool DeleteEvent(Guid id)
+    {
+        var obj = events.FirstOrDefault(e => e.Id == id);
+        if (obj == null)
+            throw new KeyNotFoundException($"Мероприятие с ID {id} не найдено");
+        return events.Remove(obj);
+    }
+
+    public IEnumerable<Event> GetAllEvents()
+    {
+        return events;
+    }
+
+    public Event GetEventById(Guid id)
+    {
+        var @event= events.FirstOrDefault(e => e.Id == id);
+        if(@event==null)
+            throw new KeyNotFoundException($"Мероприятие с ID {id} не найдено");
+        return @event;
+    }
+
+    public Event UpdateEvent(Guid id, Event newObj)
+    {
+        var oldObj = events.FirstOrDefault(e => e.Id == id);
+        if (oldObj == null)
+            throw new KeyNotFoundException($"Мероприятие с ID {id} не найдено");
+
+        oldObj.Title = newObj.Title;
+        oldObj.Description = newObj.Description;
+        oldObj.StartAt = newObj.StartAt;
+        oldObj.EndAt = newObj.EndAt;
+        return oldObj;
+    }
+}
