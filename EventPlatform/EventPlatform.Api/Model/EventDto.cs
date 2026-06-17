2b7c16cef9b61a8396d3c4230705f5e486a1ceb5
Igor Baranov <ibaranov@fluxbit.ru>
2026-06-17 16:04:52 +0300

    Поле Description необязательно

--- EventPlatform/EventPlatform.Api/Model/EventDto.cs
+++ EventPlatform/EventPlatform.Api/Model/EventDto.cs
@@ -8,9 +8,9 @@ public class EventDto
     public required Guid Id { get; init; }
     [Required(AllowEmptyStrings = false, ErrorMessage = "Название обязательно для заполнения")]
     public required string Title { get; set; }
-    public string Description { get; set; }
-    [Range(typeof(DateTime), "2026-01-01", "2026-12-31",
-        ErrorMessage = "Дата должна быть за 2026г.")]
+    public string? Description { get; set; }
+    //[Range(typeof(DateTime), "2026-01-01", "2026-12-31",
+    //    ErrorMessage = "Дата должна быть за 2026г.")]
     //public required DateTime StartAt { get; set; }
     public required DateTime StartAt
     {
@@ -23,8 +23,8 @@ public class EventDto
             field = value;
         }
     }
-    [Range(typeof(DateTime), "2026-01-01", "2026-12-31",
-        ErrorMessage = "Дата должна быть за 2026г.")]
+    //[Range(typeof(DateTime), "2026-01-01", "2026-12-31",
+    //    ErrorMessage = "Дата должна быть за 2026г.")]
     //public required DateTime EndAt { get; set; }
     public required DateTime EndAt
     {
